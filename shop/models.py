89c91efccf9a5dd3153e17dcd7bf103610f6d048
Oleh <smolynets@gmail.com>
2017-09-14 09:45:29 +0300

    add Flower model

--- shop/models.py
+++ shop/models.py
@@ -3,4 +3,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Flower(models.Model):
+
+	class Meta(object):
+		verbose_name = u'Квітка'
+		verbose_name_plural = u'Квіти'
+
+	title = models.CharField(
+      max_length=256,
+      blank=False)
+	lat_title = models.CharField(
+      max_length=256,
+      blank=False)
+	price = models.CharField(
+      max_length=256,
+      blank=False)
+	description = models.TextField(
+      blank=False)
+	photo_main = models.ImageField(
+      blank=False,
+      null=False)
+	photo_big = models.ImageField(
+      blank=True,
+      null=True)
+	def __unicode__(self):
+		return u'%s' % (self.title)
