1646b5c1a21afb86b6f3eb7151b1eab5e38194ff
Oleh <smolynets@gmail.com>
2017-09-16 07:36:28 +0300

    main_page: pagination

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Flower
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 
@@ -10,6 +11,15 @@ from .models import Flower
 ###########################################################################
 def main_page(request):
 	flowers = Flower.objects.all()
+	#pagination
+	paginator = Paginator(flowers, 4)
+	page = request.GET.get('page')
+	try:
+		flowers = paginator.page(page)
+	except PageNotAnInteger:
+		flowers = paginator.page(1)
+	except EmptyPage:
+		flowers= paginator.page(paginator.num_pages)
 	return render(request, 'shop/main.html', {'flowers': flowers})
   
 
