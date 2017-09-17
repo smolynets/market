433fd3e66d983d90f23a293a5f679e4ce9543c23
oleh Smolynets <smolynets@gmail.com>
2017-09-17 21:40:11 +0300

    add sorting of flowers

--- shop/views.py
+++ shop/views.py
@@ -10,16 +10,21 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 ###########################################################################
 def main_page(request):
-	flowers = Flower.objects.all()
-	#pagination
-	paginator = Paginator(flowers, 4)
-	page = request.GET.get('page')
-	try:
-		flowers = paginator.page(page)
-	except PageNotAnInteger:
-		flowers = paginator.page(1)
-	except EmptyPage:
-		flowers= paginator.page(paginator.num_pages)
-	return render(request, 'shop/main.html', {'flowers': flowers})
-  
+  flowers = Flower.objects.all()
+  # try to order flowers list
+  order_by = request.GET.get('order_by', '')
+  if order_by in ('price', '#'):
+    flowers = flowers.order_by(order_by)
+    if request.GET.get('reverse', '') == '1':
+      flowers = flowers.reverse()
+  #pagination
+  paginator = Paginator(flowers, 4)
+  page = request.GET.get('page')
+  try:
+    flowers = paginator.page(page)
+  except PageNotAnInteger:
+    flowers = paginator.page(1)
+  except EmptyPage:
+    flowers= paginator.page(paginator.num_pages)
+  return render(request, 'shop/main.html', {'flowers':flowers})
 
