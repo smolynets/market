d979d45648b52345c2cb0228fec64764b217383b
Oleh <smolynets@gmail.com>
2017-09-21 13:09:14 +0300

    basket_page

--- cart/urls.py
+++ cart/urls.py
@@ -11,6 +11,9 @@ from shop.views import FlowerDelete
 urlpatterns = [
     
     url(r'^add/(?P<flower_id>\d+)/$', views.CartAdd, name='CartAdd'),
+    url(r'^basket', views.basket, name='basket'),
+    url(r'^ad/(?P<flower_id>\d+)/$', views.CartAdd_basket, name='CartAdd_basket'),
+    url(r'^remove/(?P<flower_id>\d+)/$', views.CartRemove, name='CartRemove'),
     
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect, get_object_or_404
 from shop.models import Flower
 from .cart import Cart
+from .forms import CartAddProductForm
 
 
 
@@ -13,6 +14,40 @@ def CartAdd(request, flower_id):
     cart.add(flower=flower, quantity=1)
     return redirect('main')
 
-
-
 #######################################################################
+
+
+
+def basket(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+                                        initial={
+                                            'quantity': item['quantity'],
+                                            'update': True
+                                        })
+    return render(request, 'shop/basket.html', {'cart': cart})
+
+#####################################################################
+
+
+def CartAdd_basket(request, flower_id):
+    cart = Cart(request)
+    flower = get_object_or_404(Flower, id=flower_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(flower=flower, quantity=cd['quantity'],
+                                  update_quantity=cd['update'])
+    return redirect('basket')
+
+#########################################################################
+
+def CartRemove(request, flower_id):
+    cart = Cart(request)
+    flower = get_object_or_404(Flower, id=flower_id)
+    cart.remove(flower)
+    return redirect('basket')
+
+
+#############################################################################
