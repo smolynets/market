d6442492de7651c13c7fe59a5e9333f363357bee
Oleh <smolynets@gmail.com>
2017-09-24 20:05:04 +0300

    registration(full)

--- shopdb/settings.py
+++ shopdb/settings.py
@@ -132,3 +132,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 CART_SESSION_ID = 'cart'
 
 REGISTRATION_OPEN = True
+
+
+
+
+# email settings
+ADMIN_EMAIL = 'shopdb.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '465'
+EMAIL_HOST_USER = 'smolynets1@gmail.com'
+EMAIL_HOST_PASSWORD = 'dobrosyno'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+
+
+ACCOUNT_ACTIVATION_DAYS = 1
