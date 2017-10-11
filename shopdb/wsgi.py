2add56de8f36283f6fd55a08702500c0eade582f
oleh <smolynets@gmail.com>
2017-10-11 20:47:14 +0300

    wsgi.py+

--- shopdb/wsgi.py
+++ shopdb/wsgi.py
@@ -1,16 +1,21 @@
 """
-WSGI config for shopdb project.
+WSGI config for osbb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+root = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, root)
+packages = '/var/www/.virtualenvs/oleh/lib/python2.7/site-packages'
+sys.path.insert(0, packages)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shopdb.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
